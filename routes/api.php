751c8f8f90948b9e67901c0fe3e709632bb627dc
João Gabriel Alves Pereira da Silva <joaogabrielapsss@gmail.com>
2025-06-23 13:44:33 -0300

    feat: add API routes and console commands

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\StarWarsController;
+use App\Http\Controllers\Api\StatisticsController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1/starwars')->group(function () {
@@ -10,11 +11,15 @@ Route::prefix('v1/starwars')->group(function () {
     Route::prefix('people')->group(function () {
         Route::get('/', [StarWarsController::class, 'getPeople']);
         Route::get('/{id}', [StarWarsController::class, 'getPerson'])->whereNumber('id');
+        Route::get('/{id}/basic', [StarWarsController::class, 'getPersonBasic'])->whereNumber('id');
+        Route::get('/{id}/films', [StarWarsController::class, 'getPersonFilms'])->whereNumber('id');
     });
 
     Route::prefix('films')->group(function () {
         Route::get('/', [StarWarsController::class, 'getFilms']);
         Route::get('/{id}', [StarWarsController::class, 'getFilm'])->whereNumber('id');
+        Route::get('/{id}/basic', [StarWarsController::class, 'getFilmBasic'])->whereNumber('id');
+        Route::get('/{id}/characters', [StarWarsController::class, 'getFilmCharacters'])->whereNumber('id');
     });
 
     Route::prefix('starships')->group(function () {
@@ -40,4 +45,11 @@ Route::prefix('v1/starwars')->group(function () {
     // Search endpoint
     Route::get('search/{resource}', [StarWarsController::class, 'search'])
         ->whereIn('resource', ['people', 'films', 'starships', 'vehicles', 'species', 'planets']);
+});
+
+// Statistics endpoints
+Route::prefix('v1/statistics')->group(function () {
+    Route::get('/', [StatisticsController::class, 'index']);
+    Route::get('/summary', [StatisticsController::class, 'summary']);
+    Route::get('/{type}', [StatisticsController::class, 'show']);
 });
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Jobs\ComputeQueryStatisticsJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new ComputeQueryStatisticsJob)
+    ->everyFiveMinutes()
+    ->name('compute-query-statistics')
+    ->withoutOverlapping()
+    ->onOneServer();
